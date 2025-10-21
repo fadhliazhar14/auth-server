ff05eaccdeb3d5bd0e9afcdd95ec3754ca00a010
fadhliazhar14 <fadhliazhar44@gmail.com>
2025-10-21 22:49:13 +0700

    - Add new exception - Add role claims for token - Add token issuer config property

--- src/main/java/com/fadhli/auth_server/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fadhli/auth_server/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.fadhli.auth_server.exception;
 
 import com.fadhli.auth_server.util.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse<?>> handleResourceNotFound(
@@ -55,5 +59,28 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponse.error(HttpStatus.BAD_REQUEST.value(), "Validation failed", errorDetails));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleGenericException(
+            Exception ex, HttpServletRequest request) {
+
+        log.error("Unexpected error on path: {} - Error: {}",
+                request.getRequestURI(), ex.getMessage(), ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        "An unexpected error occurred", null));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiResponse<?>> handleBadCredentials(BadCredentialsException ex) {
+        ApiResponse<?> response = ApiResponse.error(
+                HttpServletResponse.SC_UNAUTHORIZED,
+                "Invalid username or password",
+                null
+        );
+
+        return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(response);
     }
 }
