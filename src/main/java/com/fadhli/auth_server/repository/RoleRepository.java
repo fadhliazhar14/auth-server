1aa7671f33ef143fe56c92a70f33fc2537aba0b0
fadhliazhar14 <fadhliazhar44@gmail.com>
2025-10-23 15:22:46 +0700

    Add pagination for roles

--- src/main/java/com/fadhli/auth_server/repository/RoleRepository.java
+++ src/main/java/com/fadhli/auth_server/repository/RoleRepository.java
@@ -1,11 +1,25 @@
 package com.fadhli.auth_server.repository;
 
 import com.fadhli.auth_server.entity.Role;
+import com.fadhli.auth_server.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Boolean existsByName(String name);
     Boolean existsByNameAndIdNot(String name, Long id);
+
+    @Query("""
+            SELECT r
+            FROM Role r
+            WHERE :search IS NULL OR :search = ''
+            OR r.name LIKE CONCAT(:search, '%')
+            OR r.description LIKE CONCAT(:search, '%')
+            """)
+    Page<Role> findAllWithPagination(@Param("search") String search, Pageable pageable);
 }
