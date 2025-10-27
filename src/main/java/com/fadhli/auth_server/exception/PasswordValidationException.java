47d06f99d91fc22f1c6208af862747ab3119ef86
fadhliazhar14 <fadhliazhar44@gmail.com>
2025-10-27 11:28:53 +0700

    Add getter annotation in PasswordValidationException

--- src/main/java/com/fadhli/auth_server/exception/PasswordValidationException.java
+++ src/main/java/com/fadhli/auth_server/exception/PasswordValidationException.java
@@ -1,16 +1,15 @@
 package com.fadhli.auth_server.exception;
 
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
 public class PasswordValidationException extends RuntimeException {
     private final List<String> errors;
 
     public PasswordValidationException(List<String> errors) {
         super("Password validation failed");
         this.errors = errors;
-    }
-
-    public List<String> getErrors() {
-        return errors;
     }
 }
